8931ac750bb9be27d91e02fe8359710ab73a4305
Shreyas Narasimhan <shreyas.narasimhan2002@gmail.com>
2026-01-12 13:00:58 +0530

    Inital Commit

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,14 +1,16 @@
 package runner;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
-        glue = {"utility", "stepDefinition" },
-        plugin = {"pretty", "html:target/cucumber-html-report/index.html","json:target/cucumber.json" }
+        glue = {"utility", "stepDefinition"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-html-report/index.html",
+                "json:target/cucumber.json"
+        }
 )
-public class Runner {
-
+public class Runner extends AbstractTestNGCucumberTests {
 }
